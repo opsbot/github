b8327cf5083d8240224384a886241fe9e120467b
OpsBots <47907782+opsbotdev@users.noreply.github.com>
2019-02-22 22:26:19 -0600

    initial commit

---
+++ cli/commands.go
@@ -0,0 +1,12 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// AddCommands add the commands from subcommand groups to the root command
+func AddCommands(cmd *cobra.Command) {
+	cmd.AddCommand(
+		VersionCommand(),
+	)
+}
